22ad7b33fffce21188ea31bebe978501a2dfba92
xiongbin <01549685@yto.net.cn>
2019-07-24 14:38:05 +0800

    test2

--- app/src/main/java/com/github/nkzawa/socketio/androidchat/User.java
+++ app/src/main/java/com/github/nkzawa/socketio/androidchat/User.java
@@ -3,7 +3,7 @@ package com.github.nkzawa.socketio.androidchat;
 public class User {
     public String username;
     public String password;
-
+    //test2
     public User(String username, String password) {
         this.username = username;
         this.password = password;
